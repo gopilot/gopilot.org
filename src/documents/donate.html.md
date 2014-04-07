59dcae60a5585d1a28d36c6cf2e89cc1e7f5a5fc
Gabe Boning <gboning@gmail.com>
2014-04-07 16:16:45 -0400

    donate page and some mobile font sizes

---
+++ src/documents/donate.html.md
@@ -0,0 +1,4 @@
+---
+layout: donate
+isPage: true
+---
--- src/documents/js/script.js
+++ src/documents/js/script.js
@@ -1,5 +1,6 @@
 var content = document.querySelector('.container');
 var duplicate = content.cloneNode(true);
+duplicate.removeAttribute("id");
 var contentBlurred = document.createElement('div');
 contentBlurred.className = 'content-blurred';
 contentBlurred.appendChild(duplicate);
@@ -12,9 +13,7 @@ translation;
 
 contentWrapper.addEventListener('scroll',function(){
     if($(window).width() < 800) return;
-    console.log('scroll');
   translation = 'translate3d(0,' + (-$(window).scrollTop() + 'px') + ',0)';
-  console.log(translation);
   duplicate.style['-webkit-transform'] = translation;
   duplicate.style['-moz-transform'] = translation;
   duplicate.style['transform'] = translation;
@@ -40,3 +39,59 @@ $(contentBlurred).css({
 });
 
 $(window).resize();
+
+
+stripeKey = "pk_live_BXRjo7MBwBvPSNM1338ZQVj3";
+paymentsServer = "http://payments.gopilot.org";
+
+var finished = function() {
+    console.log("DONE", arguments);
+    $("#main #donate").hide();
+    $("#main .success").text("Thank you! Your receipt should arrive in your inbox soon.").show();
+    $("#main .status").hide();
+};
+
+var error = function(res) {
+    var err = JSON.parse(res.responseText);
+    console.log(arguments);
+    $("#main .error").text("WHOOPS: " + err.message);
+
+    $("#main .status").hide();
+};
+
+var handler = StripeCheckout.configure({
+    key: stripeKey,
+    image: "/img/logo_square.png",
+    token: function(token, args) {
+        console.log("stripe callback", token, total);
+        $.ajax({
+            url: paymentsServer,
+            type: "POST",
+            data: {
+                card: token.id,
+                amount: total,
+                email: token.email
+            },
+            success: finished,
+            error: error,
+        });
+        $("#main .status").text("Processing...").show();
+    },
+});
+
+$("#main form").submit(function(e) {
+    e.preventDefault();
+    total = parseFloat($("#main .donation-amount").val()) * 100;
+    handler.open({
+        	name: "Pilot",
+			description: "Donation",
+			amount: total,
+            email: $("#main .email").val(),
+    });
+    /*Stripe.card.createToken({
+        number: $('#main .card-number').val(),
+        cvc: $('#main .card-cvc').val(),
+        exp_month: $('#main .card-expiry-month').val(),
+        exp_year: $('#main .card-expiry-year').val()
+    }, stripeResponseHandler);*/
+});
